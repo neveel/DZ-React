8c5d97131b72551506ae371599349920e90878a7
Neveel <neevel.ru@mail.ru>
2022-03-27 19:43:20 +0500

    DZ1-REACT 27.03.2022

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { App } from "./App";
+
+// 2. Создать компонент Message, отображающий переданный ему
+// пропсом текст.
+
+const reactProps = (
+  <div className="reactProps">
+    <h2>React</h2>
+  </div>
+);
+
+const FunctionMessage = () => {
+  return reactProps;
+};
+
+// 3. Изменить компонент App так, чтобы
+// тот рендерил Message и передавал ему пропсом текст (константу).
+// {/* <App react={reactProps} />
+// export function App(props) {
+//   return (
+//     <div className="App">
+//       <header className="App-header">hello {props.react} </header>
+//     </div>
+//   );
+// } */}
+
+// 5. Установить расширение React Devtools.
+// Установил в браузере хром, ссылка была в методичке
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <FunctionMessage />
+    <hr />
+    <App react={reactProps} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
